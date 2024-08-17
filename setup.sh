944378004be3beeb108e806f528a84395930a4fb
Whoami-X-imin <rubalsharma507@gmail.com>
2024-08-17 12:48:40 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+echo Setup Starting........
+echo ""
+sudo gem install net-ping
+echo ""
+sudo gem install colorize
+echo ""
+sudo gem install optparse
+echo ""
+sudo gem install  timeout
+
+echo ""
+
+echo Setup Complete GO AHEAD!!!!!!
